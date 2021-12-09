aef79e073d1dcbd27f1b1cf7d8e811b7231d3244
Alex Orlet <AlexOrlet89@gmail.com>
2021-12-09 15:22:35 -0800

    added button functions

--- app.js
+++ app.js
@@ -3,17 +3,41 @@
 import { score } from "./game.js";
 import { generateThrow } from "./game.js";
 
-// grab DOM elements
-const button = document.getElementById('rock');
+const buttonRock = document.getElementById('rock');
+const buttonPaper = document.getElementById('paper');
+const buttonScissors = document.getElementById('scissors');
 const resultsEl = document.getElementById('results');
-button.addEventListener('click', () => {
+
+buttonRock.addEventListener('click', () => {
   const computer = generateThrow();
   console.log(computer);
-  const player = button.id;
+  const player = buttonRock.id;
   const results = score(player, computer);
   console.log(results);
   resultsEl.textContent = results;
 });
+
+
+buttonPaper.addEventListener('click', () => {
+  const computer = generateThrow();
+  console.log(computer);
+  const player = buttonPaper.id;
+  const results = score(player, computer);
+  console.log(results);
+  resultsEl.textContent = results;
+});
+
+
+buttonScissors.addEventListener('click', () => {
+  const computer = generateThrow();
+  console.log(computer);
+  const player = buttonScissors.id;
+  const results = score(player, computer);
+  console.log(results);
+  resultsEl.textContent = results;
+});
+
+// grab DOM elements
 // document.getElementById('paper').addEventListener('click');
 // document.getElementById('scissors').addEventListener('click');
 
