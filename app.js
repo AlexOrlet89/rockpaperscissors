34ede2708d9fda3bf67a9195ce828763c3833135
Alex Orlet <AlexOrlet89@gmail.com>
2021-12-09 17:26:48 -0800

    fixed it all

--- app.js
+++ app.js
@@ -3,43 +3,71 @@
 import { score } from "./game.js";
 import { generateThrow } from "./game.js";
 
+
+
+
+
+
+const dwaynePicks = document.getElementById('dwaynepicks');
 const buttonRock = document.getElementById('rock');
+const images = (computer) => {
+  const rockpic = 'assets/rock.jpg';
+  const paperpic = 'assets/paper.jpg';
+  const scissorspic = 'assets/scissors.jpg';
+  if (computer === 'rock') { return rockpic; }
+  if (computer === 'paper') { return paperpic; }
+  if (computer === 'scissors') { return scissorspic; }
+
+};
+
+console.log(images('rock'));
+
+
+
+
 const buttonPaper = document.getElementById('paper');
 const buttonScissors = document.getElementById('scissors');
 const resultsEl = document.getElementById('results');
+const computerChooses = document.getElementById('computerchooses');
 
 buttonRock.addEventListener('click', () => {
   const computer = generateThrow();
-  console.log(computer);
   const player = buttonRock.id;
   const results = score(player, computer);
-  console.log(results);
   resultsEl.textContent = results;
+  computerChooses.textContent = ('Dwayne the Rock johnson chooses... ' + computer);
+  // dwaynePicks.src = `/assets/${computer}.jpg`;
+  const computerImage = images(computer);
+  dwaynePicks.src = computerImage;
 });
 
 
 buttonPaper.addEventListener('click', () => {
   const computer = generateThrow();
-  console.log(computer);
   const player = buttonPaper.id;
   const results = score(player, computer);
-  console.log(results);
   resultsEl.textContent = results;
+  computerChooses.textContent = ('Dwayne the Rock johnson chooses... ' + computer);
+  const computerImage = images(computer);
+  dwaynePicks.src = computerImage;
 });
 
 
 buttonScissors.addEventListener('click', () => {
   const computer = generateThrow();
-  console.log(computer);
   const player = buttonScissors.id;
   const results = score(player, computer);
-  console.log(results);
   resultsEl.textContent = results;
+  computerChooses.textContent = ('Dwayne the Rock johnson chooses... ' + computer);
+  const computerImage = images(computer);
+  dwaynePicks.src = computerImage;
 });
 
 // grab DOM elements
 // document.getElementById('paper').addEventListener('click');
 // document.getElementById('scissors').addEventListener('click');
+
+// if (computer = 'rock') {
 
 
 
--- game.js
+++ game.js
@@ -3,13 +3,13 @@ export function score(player, computer) {
         return 'tie';
     }
     if (player === 'scissors' && computer === 'rock') {
-        return 'computer wins';
+        return 'Dwayne wins';
     }
     if (player === 'scissors' && computer === 'paper') {
         return 'player wins';
     }
     if (player === 'rock' && computer === 'paper') {
-        return 'computer wins';
+        return 'Dwayne wins';
     }
     if (player === 'rock' && computer === 'scissors') {
         return 'player wins';
@@ -18,7 +18,7 @@ export function score(player, computer) {
         return 'player wins';
     }
     if (player === 'paper' && computer === 'scissors') {
-        return 'computer wins';
+        return 'Dwayne wins';
     }
 
 }
